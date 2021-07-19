251cd10e036e917144f3dc1cfe2946cdc4f3a1cd
somenerd3 <sammbogo65@gmail.com>
2021-07-19 13:47:19 +0300

    fixed add to cart buttons

--- store/views.py
+++ store/views.py
@@ -1,6 +1,11 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+
+from cart.views import _cart_id
+from django.http import HttpResponse
+from cart.models import CartItem
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -23,11 +28,13 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
 
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
 
     return render(request, 'store/product_detail.html', context)
